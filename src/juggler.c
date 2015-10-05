250fce4b58584543da63541ef9345fcf32c1304b
Taylor Hornby <havoc@defuse.ca>
2015-10-05 11:07:04 -0600

    Add benchmarking code.

--- src/juggler.c
+++ src/juggler.c
@@ -1,11 +1,22 @@
 #include <stdio.h>
+#include <sys/time.h>
+#include <sys/resource.h>
 
 #include "proofofwork.h"
+
+double get_time()
+{
+    struct timeval t;
+    gettimeofday(&t, NULL);
+    return t.tv_sec + t.tv_usec*1e-6;
+}
 
 int main(int argc, char **argv)
 {
     puzzle_t puzzle;
     solution_t solution;
+    double start_time;
+    int ret;
 
     /* We use pointer hacks to hash the buckets. If they contain padding, the
      * proof-of-work function becomes insecure, because the prover can twiddle
@@ -17,10 +28,19 @@ int main(int argc, char **argv)
     printf("Puzzle size: %zu\n", sizeof(puzzle_t));
     printf("Solution size: %zu\n", sizeof(solution));
 
+    start_time = get_time();
     juggler_create_puzzle(&puzzle);
-    juggler_find_solution(&puzzle, &solution);
+    printf("Time to create a puzzle: %.5f\n", get_time() - start_time);
 
-    if (juggler_check_solution(&puzzle, &solution)) {
+    start_time = get_time();
+    juggler_find_solution(&puzzle, &solution);
+    printf("Time to find a solution: %.5f\n", get_time() - start_time);
+
+    start_time = get_time();
+    ret = juggler_check_solution(&puzzle, &solution);
+    printf("Time to check a solution: %.5f\n", get_time() - start_time);
+
+    if (ret) {
         printf("Solution found.\n");
     } else {
         printf("Solution is wrong (BUG!)\n");
